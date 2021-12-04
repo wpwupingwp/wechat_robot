93a87af23b9ee328313caa46047b043493d25c05
Ping Wu <wpwupingwp@outlook.com>
2021-12-04 11:33:40 +0800

    rename

--- record_msg.py
+++ record_msg.py
@@ -7,15 +7,15 @@ import itchat
 from get_userid import get_user_id
 
 
-record_file = Path('record.txt').absolute()
-handle = open(record_file, 'a', encoding='utf-8')
-
 itchat.login()
 itchat.send('Hello world', toUserName='filehelper')
 me = itchat.search_friends()['UserName']
 # some group name is hard to get
 get_user_id()
 room_id = input('要记录的群聊ID：')
+room_name = itchat.search_chatrooms(userName=room_id)['NickName']
+record_file = Path(f'record-{room_name}.txt').absolute()
+handle = open(record_file, 'a', encoding='utf-8')
 @itchat.msg_register([itchat.content.TEXT, ], isGroupChat=True)
 def record(msg):
     if msg['ToUserName'] != room_id:
